5b7c78800e681cf3f2642941cc9bf58688235e06
PrawiraGenestonlia <prawira.96@gmail.com>
2019-09-04 11:10:25 +0800

    .

---
+++ index.js
@@ -0,0 +1,43 @@
+const config = require("config");
+// const mongoose = require("mongoose");
+
+//
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+//routes
+const morphRoute = require("./routes/morph.route");
+
+
+
+//use config module to get the privatekey, if no private key set, end the application
+if (!config.get("myprivatekey")) {
+  console.error("FATAL ERROR: myprivatekey is not defined.");
+  process.exit(1);
+}
+
+//connect to mongodb
+// mongoose
+//   .connect(process.env.MONGODB_URL, { useNewUrlParser: true })
+//   .then(() => console.log("Connected to MongoDB..."))
+//   .catch(err => console.error("Could not connect to MongoDB..."));
+
+app.use(cors());
+app.use(bodyParser.json());
+// app.use(express.json());
+
+app.use("/status", (req, res) => {
+  res.json({ status: "alive" });
+})
+//use users route for api/users
+app.use('/api/morph', morphRoute);
+
+// print health
+// setInterval(() => { console.log(Date()) }, 1000);
+
+const port = process.env.PORT || 3810;
+app.listen(port || 3810, () => {
+  console.log(`Listening on port ${port}...`)
+});
---
+++ routes/morph.route.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+const util = require('util')
+
+
+router.get("/", async (req, res) => {
+  res.status(200).send({ status: "alive" });
+});
+
+router.post("/combine"), async (req, res) => {
+  //do sth
+  var response;
+  var url = req.body.url;
+  response.url = url;
+  //node-readability
+  res.status(200).send(response);
+}
+
+module.exports = router; 
