96241d047e6273d3fefc55faf9a486b7fcd3d66e
PrawiraGenestonlia <prawira.96@gmail.com>
2019-09-04 19:13:24 +0800

    .

--- routes/morph.route.js
+++ routes/morph.route.js
@@ -20,14 +20,4 @@ router.post("/combine"), async (req, res) => {
   res.status(200).send(response);
 }
 
-app.post('/api/images', parser.single("image"), (req, res) => {
-  console.log(req.file) // to see what is returned to you
-  const image = {};
-  image.url = req.file.url;
-  image.id = req.file.public_id;
-  Image.create(image) // save image information in database
-    .then(newImage => res.json(newImage))
-    .catch(err => console.log(err));
-});
-
 module.exports = router; 
