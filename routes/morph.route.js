eee06c8d6266265c5660c51713e7af315f4bc566
PrawiraGenestonlia <prawira.96@gmail.com>
2019-09-04 19:21:23 +0800

    .

--- routes/morph.route.js
+++ routes/morph.route.js
@@ -12,10 +12,13 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/combine"), async (req, res) => {
+  var first_image = req.body.first_image;
+  var second_image = req.body.second_image;
+  console.log("first_image", first_image);
+  console.log("second_image", second_image);
   //do sth
   var response;
-  var url = req.body.url;
-  response.url = url;
+  response.message = "Successfully";
   //node-readability
   res.status(200).send(response);
 }
