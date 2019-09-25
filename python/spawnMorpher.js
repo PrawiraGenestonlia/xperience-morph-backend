e23d8802e6b76b378bbf335cc094c225b87e35b5
PrawiraGenestonlia <prawira.96@gmail.com>
2019-09-25 04:04:52 +0000

    .

--- python/spawnMorpher.js
+++ python/spawnMorpher.js
@@ -6,21 +6,20 @@ async function spawn() {
     return spawn('python', ["-u", path.join(__dirname, "face_morpher.py"), path.join(__dirname, "test1.jpg"), path.join(__dirname, "test2.jpg")]);
   }
 
-  const subprocess = runScript()
+  const subprocess = runScript();
 
   return new Promise(function (resolve, reject) {
     // print output of script
     subprocess.stdout.on('data', (data) => {
-      console.log(`data:${data}`);
-      resolve(1);
+      if (data == "Success") {
+        console.log(`data:${data}`);
+        resolve(1);
+      }
+      else {
+        console.log(`error:${data}`);
+        reject(data);
+      }
     });
-    subprocess.stderr.on('data', (data) => {
-      console.log(`error:${data}`);
-      reject(data);
-    });
-    // subprocess.stderr.on('close', () => {
-    //   console.log("Closed");
-    // });
   });
 }
 
